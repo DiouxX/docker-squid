7186918809decb06be336c4b032b6bccd45f22aa
DiouxX <github@diouxx.be>
2019-04-23 21:24:49 +0200

    Update file to use Squid with debian 9

--- Dockerfile
+++ Dockerfile
@@ -6,18 +6,20 @@ LABEL maintainer="DiouxX - github@diouxx.be"
 #Don't ask questions during install
 ENV DEBIAN_FRONTEND noninteractive
 
+#By default, Squid is on open proxy
+ENV LDAP_ENABLE=false
+
 #Install Squid3
 RUN apt update \
-&& apt -y upgrade \
-&& apt -y install nano ca-certificates squid3
+&& apt -y install ca-certificates squid3
 
 #Ports
 EXPOSE 3128
 
-VOLUME ["/etc/squid3"]
+VOLUME ["/etc/squid"]
 
 #Copy and run install script
-COPY ldap.config /opt/
+#COPY ldap.config /opt/
 COPY squid-start.sh /opt/
 RUN chmod +x /opt/squid-start.sh
 ENTRYPOINT ["/opt/squid-start.sh"]
--- squid-start.sh
+++ squid-start.sh
@@ -4,28 +4,28 @@ function squid_config() {
 
 	#Comment user deny to open proxy
         echo "Comment http_access deny all"
-        sed -i "s/^\(http_access deny all\)/# \1/g" /etc/squid3/squid.conf
+        sed -i "s/^\(http_access deny all\)/# \1/g" /etc/squid/squid.conf
 
 
         #If set to off, Squid will dont append your client's IP address
         echo "forwarded_for off"
-        echo "forwarded_for off" >> /etc/squid3/squid.conf
+        echo "forwarded_for off" >> /etc/squid/squid.conf
 
 	#Copy LDAP config file
-	echo "Copy ldap.conf -> /etc/squid3"
-        cp /opt/ldap.config /etc/squid3/
+	#echo "Copy ldap.conf -> /etc/squid3"
+        #cp /opt/ldap.config /etc/squid3/
 
         #Loading LDAP variable
-        source /etc/squid3/ldap.config
+        #source /etc/squid3/ldap.config
 
         if $LDAP_ENABLE
         then
 		echo "LDAP Enable"
-		echo "LDAP Port = $LDAP_PORT"
+		echo "LDAP Port = ${LDAP_PORT}"
 
 		case $LDAP_PORT in
 		389)
-                	echo -e "auth_param basic program /usr/lib/squid3/basic_ldap_auth -b $LDAP_DN -f $LDAP_ATTRIBUT -h $LDAP_HOST:$LDAP_PORT\nauth_param basic children 5\nauth_param basic realm $PROXY_NAME\nauth_param basic credentialsttl 2 hours\nacl ldapauth proxy_auth REQUIRED\nacl authenticated proxy_auth REQUIRED\nhttp_access allow ldapauth" >> /etc/squid3/squid.conf
+                	echo -e "auth_param basic program /usr/lib/squid/basic_ldap_auth -b ${LDAP_DN} -f ${LDAP_ATTRIBUT} -h ${LDAP_HOST}:${LDAP_PORT}\nauth_param basic children 5\nauth_param basic realm ${PROXY_NAME}\nauth_param basic credentialsttl 2 hours\nacl ldapauth proxy_auth REQUIRED\nacl authenticated proxy_auth REQUIRED\nhttp_access allow ldapauth" >> /etc/squid/squid.conf
 			;;
 		636)
 			#check if TLS_REQCERT is present
@@ -35,40 +35,42 @@ function squid_config() {
                                 echo -e "TLS_REQCERT\tnever" >> /etc/ldap/ldap.conf
                         fi
 
-                	echo -e "auth_param basic program /usr/lib/squid3/basic_ldap_auth -b $LDAP_DN -f $LDAP_ATTRIBUT -h ldaps://$LDAP_HOST:$LDAP_PORT\nauth_param basic children 5\nauth_param basic realm $PROXY_NAME\nauth_param basic credentialsttl 2 hours\nacl ldapauth proxy_auth REQUIRED\nacl authenticated proxy_auth REQUIRED\nhttp_access allow ldapauth" >> /etc/squid3/squid.conf
+                	echo -e "auth_param basic program /usr/lib/squid/basic_ldap_auth -b ${LDAP_DN} -f ${LDAP_ATTRIBUT} -h ldaps://${LDAP_HOST}:${LDAP_PORT}\nauth_param basic children 5\nauth_param basic realm ${PROXY_NAME}\nauth_param basic credentialsttl 2 hours\nacl ldapauth proxy_auth REQUIRED\nacl authenticated proxy_auth REQUIRED\nhttp_access allow ldapauth" >> /etc/squid/squid.conf
 			;;
 		esac
         else
 		echo "LDAP Disable"
                 #Add allow directive and fowarded_for
                 echo "http_access allow all"
-                echo "http_access allow all" >> /etc/squid3/squid.conf
+                echo "http_access allow all" >> /etc/squid/squid.conf
         fi
 }
 
-#Check if /etc/squid3/squid.conf.origin exist
-if [ -f /etc/squid3/squid.conf.origin ]
-then
-        echo "File /etc/squid3/squid.conf.origin exist"
-        #Erase file config
-        cp /etc/squid3/squid.conf.origin /etc/squid3/squid.conf
-        echo "Copy squid.conf.origin -> squid.conf"
+#Check if /etc/squid/squid.conf.origin exist
+# if [ -f /etc/squid/squid.conf.origin ]
+# then
+#         echo "File /etc/squid/squid.conf.origin exist"
+#         #Erase file config
+#         cp /etc/squid/squid.conf.origin /etc/squid/squid.conf
+#         echo "Copy squid.conf.origin -> squid.conf"
 	
-        #Squid config
-        squid_config
+#         #Squid config
+#         squid_config
 
-else
-        #Backup original config file
-        echo "File /etc/squid3/squid.conf.origin doesn't exist"
-        cp /etc/squid3/squid.conf /etc/squid3/squid.conf.origin
-        echo "Copy squid.conf -> squid.conf.origin"
+# else
+#         #Backup original config file
+#         echo "File /etc/squid/squid.conf.origin doesn't exist"
+#         cp /etc/squid/squid.conf /etc/squid/squid.conf.origin
+#         echo "Copy squid.conf -> squid.conf.origin"
 	
-        #Squid config
-        squid_config
-fi
+#         #Squid config
+#         squid_config
+# fi
 
 #Delete PID file before else crash on docker restart
-rm -rf /var/run/squid3.pid
+#rm -rf /var/run/squid3.pid
+
+squid_config
 
 #Launch squid on foreground
-/usr/sbin/squid3 -NCd1 -f /etc/squid3/squid.conf
+/usr/sbin/squid -NCd1 -f /etc/squid/squid.conf
